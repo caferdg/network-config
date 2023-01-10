90231043065a82275fa63cb0221b53346075b649
yoryd <yoryyy@live.fr>
2023-01-10 22:31:50 +0100

    maj

--- conf.py
+++ conf.py
@@ -1,9 +1,13 @@
 import json
 
-f = open("conf.json", "r")
+# PREFERENCES
+lpPrefix = "2003::"
+
+# IMPORT NETWORK INTENT
+f = open("intent.json", "r")
 jsonFile = json.load(f)
-routers = jsonFile["routers"]
 f.close()
+routers = jsonFile["routers"]
 
 nbRouter = len(routers)
 nbAs = len(jsonFile["as"])
@@ -18,57 +22,139 @@ listeSousRes = [] # Liste contenant les compteurs sous-reseaux utilises pour cha
 for k in range(0,nbAs):
     listeSousRes.append(0)
 
+matAdjAs = [] # Matrice contenant les numeros d'adjacence entre AS
+for k in range(0,nbAs):
+    matAdjAs.append([])
+    for j in range(nbAs):
+        matAdjAs[k].append(0)
 
+nbAdjAs = 0 # nombre d'adjacence entre AS
+
+routersInAs = [] # liste des routeurs presents dans chaque AS
+for As in jsonFile["as"]:
+    routersInAs.append([])
+    routersInAs[As-1] = [router["id"] for router in routers if router["as"]==As]
 
 for router in routers:
 
     id = router["id"]
     igp = router["int-protocol"]
     egp = router["border-protocol"]
-    adj = router["adj"]
     As = router["as"]
+    adj = router["adj"]
+    isASBR = False
+    egpNeigbors = []
 
-    result = open("i"+ str(id) + "_startup-config.cfg", "w")
+    res = open("i"+ str(id) + "_startup-config.cfg", "w")
 
-    result.write("version 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\nhostname R"+str(id)+"\nboot-start-marker\nboot-end-marker\nno aaa new-model\nno ip icmp rate-limit unreachable\nip cef\nno ip domain lookup\nipv6 unicast-routing\nipv6 cef\nmultilink bundle-name authenticated\nip tcp synwait-time 5\n")
-    result.write("!\n!\n!\n!\n!\n")
+    ## CONSTANTS
+    res.write("version 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\nhostname R"+str(id)+"\nboot-start-marker\nboot-end-marker\nno aaa new-model\nno ip icmp rate-limit unreachable\nip cef\nno ip domain lookup\nipv6 unicast-routing\nipv6 cef\nmultilink bundle-name authenticated\nip tcp synwait-time 5\n")
+    res.write("!\n!\n!\n!\n!\n")
 
-    result.write("interface Loopback0\n no ip address\n ipv6 address 2001::"+str(id)+"/128\n ipv6 enable\n")
-
+    ## LOOPBACK
+    res.write("interface Loopback0\n no ip address\n ipv6 address "+lpPrefix+str(id)+"/128\n ipv6 enable\n")
     if(igp == "rip"):
-        result.write(" ipv6 rip ripng enable\n")
-    else:
-        result.write(" ipv6 ospf 1 area 0\n")
-    result.write("!\n")
+        res.write(" ipv6 rip ripng enable\n")
+    if(igp == "ospf"):
+        res.write(" ipv6 ospf 1 area 0\n")
+    res.write("!\n")
     
-
+    ## PHYSICAL INTERFACES
     for adj in router["adj"]:
         neighbID = adj["neighbor"]
+        neighbAs = [router["as"] for router in routers if router["id"]==neighbID][0]
 
         for link in adj["links"]:
-            if(str(link["protocol-type"]) == "igp"): # routeur a l'interieur de l'AS (pas en bordure)
-                ip = "2001:"+str(As)+"00:"+str(As)+"00:"
-                if (matAdj[id-1][neighbID-1] == 0 and matAdj[neighbID-1][id-1]==0): # sous reseau pas encore initialise
-                    listeSousRes[As-1] += 1
-                    matAdj[id-1][neighbID-1], matAdj[neighbID-1][id-1] = listeSousRes[As-1], listeSousRes[As-1]
-                    ip += str(matAdj[id-1][neighbID-1]) + "::1"
-                else: # sous reseau deja cree
-                    ip += str(matAdj[id-1][neighbID-1]) + "::2"
-            else: # routeur en bordure d'AS
-                print("en bordure")
-            result.write("interface " + str(link["interface"]) + "\n")
-            result.write(" no ip address\n")
+
+            ## IP GENERATION
+            if str(link["protocol-type"]) == "igp": # routeur a l'interieur de l'AS (pas ASBR)
+                bloc = As
+            if str(link["protocol-type"]) == "egp": # routeur en bordure d'AS
+                isASBR = True
+                if matAdjAs[As-1][neighbAs-1] == 0 and matAdjAs[neighbAs-1][As-1]==0: # adjacence inter AS pas encore initialise
+                    nbAdjAs += 1
+                    bloc = nbAs + nbAdjAs
+                    matAdjAs[As-1][neighbAs-1], matAdjAs[neighbAs-1][As-1] = bloc, bloc
+                    listeSousRes.append(0)
+                else : # adjacence inter AS connue
+                    bloc = matAdjAs[As-1][neighbAs-1]
+
+            ip = "2001:"+str(bloc)+"00:"+str(bloc)+"00:"
+
+            if matAdj[id-1][neighbID-1] == 0 and matAdj[neighbID-1][id-1]==0: # sous reseau pas encore initialise
+                listeSousRes[bloc-1] += 1
+                matAdj[id-1][neighbID-1], matAdj[neighbID-1][id-1] = listeSousRes[bloc-1], listeSousRes[bloc-1]
+                ip += str(matAdj[id-1][neighbID-1]) + "::1"
+                if isASBR and str(link["protocol-type"]) == "egp":
+                    egpNeigbors.append(ip[:-1] + "2" + " " + str(neighbAs))
+            else: # sous reseau deja cree
+                ip += str(matAdj[id-1][neighbID-1]) + "::2"
+                if isASBR and str(link["protocol-type"]) == "egp":
+                    egpNeigbors.append(ip[:-1] + "1" + " "+ str(neighbAs))
+
+            # INTERFACE
+            res.write("interface " + str(link["interface"]) + "\n")
+            res.write(" no ip address\n")
             if str(link["interface"]).startswith("FastEthernet") :
-                result.write(" duplex full\n")
+                res.write(" duplex full\n")
             if str(link["interface"]).startswith("GigabitEthernet") :
-                result.write(" negotiation auto\n")
-            result.write(" ipv6 address " + ip +"/64\n ipv6 enable\n")
+                res.write(" negotiation auto\n")
+            res.write(" ipv6 address " + ip +"/64\n ipv6 enable\n")
 
             if str(link["protocol-type"]) == "igp" :
                 if igp == "rip":
-                    result.write(" ipv6 rip ripng enable\n")
+                    res.write(" ipv6 rip ripng enable\n")
                 if igp == "ospf":
-                    result.write(" ipv6 ospf 1 area 0\n")
+                    res.write(" ipv6 ospf 1 area 0\n")
             
-            result.write("!\n")
-                    
+            res.write("!\n")
+    
+    ## EGP
+    if egp == "bgp":
+        res.write("router bgp " + str(As) + "\n")
+        res.write(" bgp router-id "+ str(id)+"."+str(id)+"."+str(id)+"."+str(id) + "\n")
+        res.write(" bgp log-neighbor-changes\n no bgp default ipv4-unicast\n")
+
+        if isASBR :
+            for ebgpNeighb in egpNeigbors:
+                ipNeighb = ebgpNeighb.split()[0]
+                asNeighb = ebgpNeighb.split()[1]
+                res.write(" neighbor "+ipNeighb +" remote-as "+ asNeighb+"\n")
+
+        for routerID in routersInAs[As-1]:
+            if routerID != id:
+                res.write(" neighbor "+lpPrefix+ str(routerID) +" remote-as "+ str(As)+"\n")
+                res.write(" neighbor "+lpPrefix+ str(routerID) +" update-source Loopback0\n")
+
+        res.write(" !\n address-family ipv4\n exit-address-family\n !\n")
+        res.write(" address-family ipv6\n")
+
+        if isASBR :
+            if(igp == "rip"):
+                res.write("  redistribute rip ripng\n")
+            if(igp == "ospf"):
+                res.write("  redistribute ospf 1\n")
+            res.write("  network 2001:"+str(As)+"00:"+str(As)+"00::/48\n")
+            for ebgpNeighb in egpNeigbors:
+                res.write("  neighbor " + ebgpNeighb.split()[0] + " activate\n")
+
+        for routerID in routersInAs[As-1]:
+            if routerID != id:
+                res.write("  neighbor "+lpPrefix+ str(routerID) +" activate\n")
+        
+        res.write(" exit-address-family\n!\n")
+
+    
+    res.write("ip forward-protocol nd\nno ip http server\nno ip http secure-server\n!\n")
+
+    ## IGP
+    if(igp == "rip"):
+        res.write("ipv6 router rip ripng\n redistribute connected\n")
+    if(igp == "ospf"):
+        res.write("ipv6 router ospf 1\n router-id "+str(id)+"."+str(id)+"."+str(id)+"."+str(id)+"\n")
+        if isASBR: # ??
+            res.write(" redistribute connected\n")
+
+    res.write("!\n")
+
+    res.write("control-plane\nline con 0\n exec-timeout 0 0\n privilege level 15\n logging synchronous\n stopbits 1\nline aux 0\n exec-timeout 0 0\n privilege level 15\n logging synchronous\n stopbits 1\nline vty 0 4\n login\n!\nend")
