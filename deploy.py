0bb72ecfbefc0fd581cc9e6e0b52a42768c28594
yoryd <yoryyy@live.fr>
2023-01-19 15:03:52 +0100

    deploy script begin

---
+++ deploy.py
@@ -0,0 +1,11 @@
+import os, sys
+
+# if len(sys.argv) != 3:
+#     print("Usage: python3 conf.py <confDir> <projectName>")
+#     exit(1)
+
+# confDir = sys.argv[1]
+# projectName = sys.argv[2]
+
+dynamipsPath = os.path.expanduser('~') + "/GNS3/projects/" + projectName + "/project-files/dynamips/"
+
